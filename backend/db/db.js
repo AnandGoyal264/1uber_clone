d27ea90266b71da510739dde97a2a2ee17efa0ea
AnandGoyal264 <anandgoyal9171@gmail.com>
2025-06-12 21:49:12 +0530

    mongodbpassword

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose');
 const connectdb = async () => {
     try {
         await mongoose.connect(
-            "mongodb+srv://anandgoyal5656:D3zoLhU2Wob56d73@cluster0.cdcdw.mongodb.net/uber?retryWrites=true&w=majority&appName=Cluster0",
+            "mongodb+srv://anandgoyal5656:GFjAJSAyZQQ8CJxr@cluster0.cdcdw.mongodb.net/uber?retryWrites=true&w=majority&appName=Cluster0",
             
           );
         console.log("✅ Connected successfully to MongoDB!");
